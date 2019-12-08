39ab5e9812fa4be1b7ab5c15c5257d55a041ca86
Rahul Ranjan <rahulranjanmca@gmail.com>
2019-12-08 23:56:50 +0530

    Cname addition

--- src/pages/index.js
+++ src/pages/index.js
@@ -180,7 +180,7 @@ function Home() {
                                 'button button--outline button--secondary button--lg',
                                 styles.getStarted,
                             )}
-                            to={useBaseUrl('docs/doc1')}>
+                            to={useBaseUrl('docs/topics')}>
                             Get Started
                         </Link>
                     </div>
